63f5bd184db201acf7c6abdffa2c52e170881729
Arthur V <vaskinel.artur@gmail.com>
2024-06-21 10:16:47 +0300

    TM-27 Connected database to server

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var database = require("./services/database.service")
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var tasksRouter = require('./routes/tasks')
 
 var app = express();
@@ -21,7 +21,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/tasks', tasksRouter)
 
 
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const router = express.Router()
 
-router.get("/", (req, res) => {
-    res.send("Tasks Array should go here")
+router.get("/", async (req, res) => {
+    res.send("user went to /tasks route")
+    
 })
 
 module.exports = router
