37a7f3fde0750aa741e397d2c005f4973152ab00
Arthur V <vaskinel.artur@gmail.com>
2024-07-09 16:40:38 +0300

    TM-32: changed location of connection to database, changed data that is being send from updateTask method

--- services/database.service.ts
+++ services/database.service.ts
@@ -1,21 +1,16 @@
 require("dotenv").config()
-import mongoose, { Error } from "mongoose";
-import { Task } from "../models/task.model";
-import { v4 as uuidv4 } from 'uuid';
-import TaskNotFoundError from "../errors/taskNotFoundError";
-import ErrorHandler from "../errors/errorHandler";
+import mongoose from "mongoose"
+import { Task } from "../models/task.model"
+import { v4 as uuidv4 } from 'uuid'
+import TaskNotFoundError from "../errors/taskNotFoundError"
+import ErrorHandler from "../errors/errorHandler"
 
-mongoose.connection.on('connected', () => console.log('connected'));
-mongoose.connection.on('open', () => console.log('open'));
-mongoose.connection.on('disconnected', () => console.log('disconnected'));
-mongoose.connection.on('reconnected', () => console.log('reconnected'));
-mongoose.connection.on('disconnecting', () => console.log('disconnecting'));
-mongoose.connection.on('close', () => console.log('close'));
-
-const uri = `${process.env.DB_CONNCETION_STRING}`
-const options = {
-    dbName: 'tasks_database',
-};
+mongoose.connection.on('connected', () => console.log('connected'))
+mongoose.connection.on('open', () => console.log('open'))
+mongoose.connection.on('disconnected', () => console.log('disconnected'))
+mongoose.connection.on('reconnected', () => console.log('reconnected'))
+mongoose.connection.on('disconnecting', () => console.log('disconnecting'))
+mongoose.connection.on('close', () => console.log('close'))
 
 const taskSchema = new mongoose.Schema({
     id: String,
@@ -24,39 +19,21 @@ const taskSchema = new mongoose.Schema({
     type: String,
     createdOn: String,
     status: String,
-});
-const TaskModel = mongoose.model('Task', taskSchema);
-
-const connectToDatabase = async () => {
-    try {
-        await mongoose.connect(uri, options)
-        console.log("Connected to MongoDB database");
-    } catch (error) {
-        console.error("Error connecting to MongoDB:", error);
-        process.exit(1);
-    }
-}
-
+})
+const TaskModel = mongoose.model('Task', taskSchema)
 
 export default class DatabaseService {
     async getAllTasks() {
-
-        await connectToDatabase();
-        return await TaskModel.find({});
-
+        return await TaskModel.find({})
     }
     async getIndividualTask(taskId: string) {
-        await connectToDatabase();
-        var task = await TaskModel.findOne({ id: taskId });
+        var task = await TaskModel.findOne({ id: taskId })
         if (task == null || task == undefined) {
             throw new TaskNotFoundError()
         }
         return task
-
     }
     async createTask(task: Task) {
-
-        await connectToDatabase();
         var generatedId = uuidv4()
         const creatingTask = await TaskModel.create({
             id: generatedId,
@@ -64,37 +41,24 @@ export default class DatabaseService {
             description: task.description,
             type: task.type,
             createdOn: new Date(),
-            status: "In progress",
+            status: "Active",
         });
         if (creatingTask) {
             return generatedId
         } else {
             throw new ErrorHandler()
         }
-
     }
     async deleteTask(taskToDelete: String) {
-
-        await connectToDatabase()
         const matchingTask = await TaskModel.exists({ id: taskToDelete })
-
         if (matchingTask) {
             await TaskModel.findOneAndDelete({ id: taskToDelete })
         } else {
             throw new TaskNotFoundError()
         }
-
-
     }
     async updateTask(updatedDataTask: Task) {
-        await connectToDatabase()
-        const matchingTask = await TaskModel.exists({ id: updatedDataTask.id })
-        if (matchingTask) {
-            await TaskModel.findOneAndUpdate({ id: updatedDataTask.id }, updatedDataTask)
-        } else {
-            throw new TaskNotFoundError()
-        }
-
+        return await TaskModel.findOneAndUpdate({ id: updatedDataTask.id }, updatedDataTask, { new: true })
     }
 }
 
